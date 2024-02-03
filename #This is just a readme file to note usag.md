096081d7a3affaca2e0bb6a4e3a65a731ad47f06
UjjawalkTjNw <ujjawal.singh467@gmail.com>
2024-02-03 21:15:54 +0530

    V2 which includes closing the popup after given duration

--- #This is just a readme file to note usag.md
+++ #This is just a readme file to note usag.md
@@ -2,3 +2,14 @@
 1. Tkinter is foundation for creating windows, creating text field, creating menus, toolbars
 2. It can be used to display information , give popups on window
 3. Can be used to handle events like popup button, clicks
+
+
+
+
+V1: Basic code that just shows notification on desktop
+    =>create a new instance of window(root)
+    =>close it (withdraw)
+V2: Shows notification for a certain duration then gets closed automatically
+    =>include a new parameter "duration"
+    =>new function to close notification after specified "duration" (if duration is 5 sec, it waits 5 sec to terminate the window/program after Ok is clicked)
+    => create a child window of root, and removed messagebox.showinfo(title, message); helpful in closing notitication after given duration
--- desktopnotify.py
+++ desktopnotify.py
@@ -1,15 +1,30 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def show_notification(title, message):
-    # create a new window for notification
-    root= tk.Tk()
-    #immmediately hide the window after notification
-    root.withdraw()
+def show_notification(title, message, duration=5000):
+    root = tk.Tk()
+    root.withdraw()  # This will prevent from creating multiple instance of window
+
+    def close_notification():
+        root.destroy()
+
+    #remove messagebox.showinfo(title,message)
+    #better make a new child window as below:
+    #create a new window that is child of root
+    new_window = tk.Toplevel(root)
+    #set the title of new window created
+    new_window.title(title)
     
-    messagebox.showinfo(title, message)
+    #Add label to show the message
+    label = tk.Label(new_window, text = message)
+    label.pack(padx=10, pady=10)
     
+    
+    
+    # Close the notification window after the duration
+    root.after(duration, close_notification)
+
+    root.mainloop()
+
 if __name__ == "__main__":
-    show_notification("Hello", "This is notification")
-        
-        
+    show_notification("Hello", "This is a notification", duration=5000)
